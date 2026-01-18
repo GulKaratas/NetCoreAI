c68902a86edeacfec59501255f9393d8df346673
GulKaratas <glsrn_7872@outlook.com>
2026-01-18 23:47:41 +0300

    Add project files.

---
+++ NetCoreAl.Project01_ApiDemo/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAl.Project01_ApiDemo.Context;
+
+namespace NetCoreAl.Project01_ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CustomersController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CustomerList() {
+            var value = _context.Customers.ToList();
+            return Ok(value);
+        }
+    }
+}
