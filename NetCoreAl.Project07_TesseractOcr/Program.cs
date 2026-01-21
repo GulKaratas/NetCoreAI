6e6d39e4bccaa6557f71784288a233171239822f
GulKaratas <glsrn_7872@outlook.com>
2026-01-21 22:46:22 +0300

    Reading text on images with Tesserect OCR.

---
+++ NetCoreAl.Project07_TesseractOcr/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Karakter okuması yapılacak resim yolu:");
+        string imagePath = Console.ReadLine();
+
+        string tessDataPath = @"C:\.Net\tessdata";
+
+        try
+        {
+            using( var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+            {
+                using( var img = Pix.LoadFromFile(imagePath))
+                {
+                    using( var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("Okunan Metin:");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Hata oluştu:  + {ex.Message}");
+
+        }
+        Console.ReadLine();
+    }
+}
