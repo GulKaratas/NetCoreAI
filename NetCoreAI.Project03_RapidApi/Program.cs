92b553f783f0f5865cc53dca18232c30f8c4ab02
GulKaratas <glsrn_7872@outlook.com>
2026-01-21 15:57:43 +0300

    Rapid Api kullanarak api çekme

---
+++ NetCoreAI.Project03_RapidApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using NetCoreAl.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "874435182bmsh630c53055d5d062p1f2f5bjsne241415a4dde" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine($"Rank: {series.rank} | Title: {series.title} | Rating: {series.rating} | Year: {series.year}");
+    }
+
+}
+
+Console.ReadLine();
---
+++ NetCoreAI.Project03_RapidApi/ViewModels/ApiSeriesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreAl.Project03_RapidApi.ViewModels
+{
+    internal class ApiSeriesViewModel
+    {
+            public int rank { get; set; }
+            public string title { get; set; }
+           
+            public float rating { get; set; }
+            
+            public string year { get; set; }
+           
+    }
+}
