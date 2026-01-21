239a69d96cbb2f72fd30e6c98094e6b8e4237135
GulKaratas <glsrn_7872@outlook.com>
2026-01-22 01:21:34 +0300

    Text to Speech

---
+++ NetCoreAl.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = -4;
+
+        Console.WriteLine("Enter text to convert to speech:");
+        string input;
+        input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input)) { 
+        speechSynthesizer.Speak(input);
+        }else
+        {
+            Console.ReadLine();
+        }
+    }
+}
