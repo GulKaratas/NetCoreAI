82d23d67ec8b5241b3a9f74965543989daf09fa1
GulKaratas <glsrn_7872@outlook.com>
2026-01-21 23:51:35 +0300

    Google Cloud Consol Json

---
+++ NetCoreAl.Project08_GoogleCloudVision/Program.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Vision.V1;
+
+class Program
+{
+       static void Main(string[] args)
+    {
+       Console.WriteLine("Resim yolunu giriniz");
+        Console.WriteLine();
+        string imagePath = Console.ReadLine();
+
+        string credentialPath = "";
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("Resimde meti:");
+            Console.WriteLine();
+            foreach (var annotation in response)
+            {
+                if (!string.IsNullOrEmpty(annotation.Description))
+                {
+                    Console.WriteLine(annotation.Description);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata oluştu: { ex.Message}");
+        }
+    }
+
+}
